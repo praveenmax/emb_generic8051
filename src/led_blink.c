dcf14d0f2ec7f68d74ec9888d9a591eca92374b8
Praveen <praveenmax@users.noreply.github.com>
2024-07-24 16:46:56 +0530

    Added led blink. Works well with 8051 trainer flashed via ISP programmer USBASP

---
+++ src/led_blink.c
@@ -0,0 +1,26 @@
+#include <8051.h>
+/*
+    LED Blink
+*/
+// Function prototype
+
+#define LED_PIN P2_0
+
+void delay(void);
+
+void main(void) {
+    while (1) {
+
+        //LED 
+        LED_PIN = 1;
+        delay();         
+        LED_PIN = 0;
+        delay();         
+
+    }
+}
+
+void delay(void) {
+    unsigned long i;
+    for (i = 0; i < 10000; i++);  // Simple delay loop
+}
